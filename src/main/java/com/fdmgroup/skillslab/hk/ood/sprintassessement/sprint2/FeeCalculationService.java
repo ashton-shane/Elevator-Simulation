8826818d802068423cc22574cbceb45bac3c5dda
Ashton Shane <ashton.shane.dev@gmail.com>
2026-03-06 15:57:33 +0800

    create FeeCalculationService Interface

---
+++ src/main/java/com/fdmgroup/skillslab/hk/ood/sprintassessement/sprint2/FeeCalculationService.java
@@ -0,0 +1,5 @@
+package com.fdmgroup.skillslab.hk.ood.sprintassessement.sprint2;
+
+public interface FeeCalculationService {
+    public void calculateFee();
+}
---
+++ src/main/java/com/fdmgroup/skillslab/hk/ood/sprintassessement/sprint2/FeeCalculatorServiceImpl.java
@@ -0,0 +1,4 @@
+package com.fdmgroup.skillslab.hk.ood.sprintassessement.sprint2;
+
+public class FeeCalculatorServiceImpl {
+}
