5e1ed431040beab933fe95e18c59556f8fd05039
Ashton Shane <ashton.shane.dev@gmail.com>
2026-03-07 19:29:53 +0800

    Assessment2: change junit dependencies imports to be in line with vers 6.0.3

--- src/test/java/sprint2/TestFeeCalculatorServiceImpl.java
+++ src/test/java/sprint2/TestFeeCalculatorServiceImpl.java
@@ -1,8 +1,10 @@
 package sprint2;
 
 import com.fdmgroup.skillslab.hk.ood.sprintassessement.sprint2.FeeCalculatorServiceImpl;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class TestFeeCalculatorServiceImpl {
@@ -10,10 +12,10 @@ public class TestFeeCalculatorServiceImpl {
     public void returnsDoubleFee_whenCalculateFee(){
         FeeCalculatorServiceImpl feeCalculator = new FeeCalculatorServiceImpl();
 
-        Assert.assertEquals(20.00, feeCalculator.calculateFee(99.00), 0.001);
-        Assert.assertEquals(15.00, feeCalculator.calculateFee(455.00), 0.001);
-        Assert.assertEquals(10.00, feeCalculator.calculateFee(599.00), 0.001);
-        Assert.assertEquals(5.00, feeCalculator.calculateFee(1789.00), 0.001);
-        Assert.assertEquals(0.00, feeCalculator.calculateFee(2000.55), 0.001);
+        assertEquals(20.00, feeCalculator.calculateFee(99.00), 0.001);
+        assertEquals(15.00, feeCalculator.calculateFee(455.00), 0.001);
+        assertEquals(10.00, feeCalculator.calculateFee(599.00), 0.001);
+        assertEquals(5.00, feeCalculator.calculateFee(1789.00), 0.001);
+        assertEquals(0.00, feeCalculator.calculateFee(2000.55), 0.001);
     }
 }
