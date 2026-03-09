0378d0704747674f3342e177df3d296564de0a27
Ashton Shane <ashton.shane.dev@gmail.com>
2026-03-09 11:11:55 +0800

    create SafetyDepositBox class

---
+++ src/main/java/com/fdmgroup/skillslab/hk/ood/sprintassessement/sprint2/SafetyDepositBox.java
@@ -0,0 +1,25 @@
+package com.fdmgroup.skillslab.hk.ood.sprintassessement.sprint2;
+
+public class SafetyDepositBox {
+    private boolean isAlloted;
+    private double id;
+
+    public double getId() {
+        return this.id;
+    }
+
+    public void setId(double id) {
+        this.id = id;
+    }
+
+    public boolean isAlloted() {
+        return this.isAlloted;
+    }
+
+    public void setAlloted(boolean alloted) {
+        this.isAlloted = alloted;
+    }
+
+
+
+}
