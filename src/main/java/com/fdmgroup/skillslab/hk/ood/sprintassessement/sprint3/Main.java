a3d880928cf7c5aeb694238a549873484a3c26f4
ruibing.lim <ruibing.lim@fdmgroup.com>
2026-03-13 01:57:07 +0800

    Update elevator service

--- src/main/java/com/fdmgroup/skillslab/hk/ood/sprintassessement/sprint3/Main.java
+++ src/main/java/com/fdmgroup/skillslab/hk/ood/sprintassessement/sprint3/Main.java
@@ -1,18 +1,12 @@
 package com.fdmgroup.skillslab.hk.ood.sprintassessement.sprint3;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     static void main(String[] args) {
         var loader = new ConfigLoader();
         loader.loadConfigFile("ElevatorConfig.txt");
-        // loader usage:
-        // loader.getConfig(); -> returns Configuration object
-        // loader.getRequests(); -> returns List<Request>
+        var requests = loader.getRequests();
+        var config = loader.getConfig();
+        var elevatorService = new ElevatorService(requests, config.get());
     }
 }
