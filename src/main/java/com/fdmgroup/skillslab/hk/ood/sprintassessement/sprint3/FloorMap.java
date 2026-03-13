0ab87294bbbe6442c6f0a3fd37b5171ce0849dce
Ashton Shane <ashton.shane.dev@gmail.com>
2026-03-13 15:55:14 +0800

    refactor floormap class

--- src/main/java/com/fdmgroup/skillslab/hk/ood/sprintassessement/sprint3/FloorMap.java
+++ src/main/java/com/fdmgroup/skillslab/hk/ood/sprintassessement/sprint3/FloorMap.java
@@ -5,11 +5,18 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class FloorMap {
     // map format: { floor : List<Passenger> }
-    Map<Integer, List<Passenger>> buildingFloorMap = new ConcurrentHashMap<>();
+    Map<Integer, List<Passenger>> buildingFloorMap;
     public static final FloorMap instance = new FloorMap();
 
     private FloorMap(){
+        buildingFloorMap = new ConcurrentHashMap<>();
+    }
 
+    public static FloorMap getInstance(){
+        return instance;
+    }
+
+    public void loadFloorMapWithPassengers(){
         for (Request r : ElevatorService.getRequests()) {
             List<Passenger> waitingPassengers = buildingFloorMap.computeIfAbsent(
                     r.getCurrentFloor(), floor -> new ArrayList<>()
@@ -22,9 +29,5 @@ public class FloorMap {
                 waitingPassengers.add(passenger);
             }
         }
-    }
-
-    public static FloorMap getInstance(){
-        return instance;
     }
 }
