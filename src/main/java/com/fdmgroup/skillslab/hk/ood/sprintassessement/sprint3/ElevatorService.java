777977b72c9e269a0eba074a35dd24c2f3bca160
ruibing.lim <ruibing.lim@fdmgroup.com>
2026-03-17 00:07:57 +0800

    Revert "Update elevator service"
    This reverts commit a3d880928cf7c5aeb694238a549873484a3c26f4.

--- src/main/java/com/fdmgroup/skillslab/hk/ood/sprintassessement/sprint3/ElevatorService.java
+++ src/main/java/com/fdmgroup/skillslab/hk/ood/sprintassessement/sprint3/ElevatorService.java
@@ -1,52 +1,12 @@
 package com.fdmgroup.skillslab.hk.ood.sprintassessement.sprint3;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class ElevatorService {
     private List<Request> requests = new ArrayList<>();
-    private Map<Integer, ArrayList<Request>> requestsMap = new HashMap<>();
     private List<Elevator> elevators = new ArrayList<>();
 
-    public ElevatorService(List<Request> requests, Configuration config) {
-        for (var r : requests) {
-            var currentList = requestsMap.getOrDefault(r.getCurrentFloor(), new ArrayList<>());
-            currentList.add(r);
-            requestsMap.put(r.getCurrentFloor(), currentList);
-        }
-    }
-
-    public void processRequests() throws InterruptedException {
-        var requests = new ArrayList<Request>();
-        var currentFloor = 0;
-        while (true) {
-            if (requests.size() == 0) {
-                requests.addAll(getRequests());
-            } else {
-                Integer smallestDiff = 10;
-                Integer targetFloor = 0;
-                for (var r : requests) {
-                    if (r.getDestinationFloor() - currentFloor < smallestDiff) {
-                        targetFloor = r.getDestinationFloor();
-                    }
-                }
-                while (currentFloor < targetFloor) {
-                    currentFloor += 1;
-                    Thread.sleep(3000);
-                }
-            }
-        }
-    }
-
-    private List<Request> getRequests() {
-        for (var pair: requestsMap.entrySet()) {
-            return pair.getValue();
-        }
-        return new ArrayList<Request>();
-    }
-    
     // methods
     public void allocateRequest(Elevator elevator) {
         int elevatorCurrentFloor = elevator.getCurrentFloor();
@@ -67,7 +27,14 @@ public class ElevatorService {
         requests.remove(request);
     }
 
+    // getters
+    public List<Request> getRequests() {
+        return requests;
+    }
+
     public List<Elevator> getElevators() {
         return elevators;
     }
+
+
 }
--- src/main/java/com/fdmgroup/skillslab/hk/ood/sprintassessement/sprint3/Main.java
+++ src/main/java/com/fdmgroup/skillslab/hk/ood/sprintassessement/sprint3/Main.java
@@ -1,12 +1,18 @@
 package com.fdmgroup.skillslab.hk.ood.sprintassessement.sprint3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     static void main(String[] args) {
         var loader = new ConfigLoader();
         loader.loadConfigFile("ElevatorConfig.txt");
-        var requests = loader.getRequests();
-        var config = loader.getConfig();
-        var elevatorService = new ElevatorService(requests, config.get());
+        // loader usage:
+        // loader.getConfig(); -> returns Configuration object
+        // loader.getRequests(); -> returns List<Request>
     }
 }
